99d03dc84044ee740e8ce38d6ad24c68cdd0345c
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-03-16 14:50:44 +0100

    Appearing Test Page instead of React Template

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css';
+import { TestPage } from './components/pages/TestPage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Savor the Anticipation: Our Website is Fermenting Something
-          Extraordinary! Stay Tuned for a Tantalizing Blend of Wine and Culinary
-          Delights.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <TestPage />;
 }
 
 export default App;
