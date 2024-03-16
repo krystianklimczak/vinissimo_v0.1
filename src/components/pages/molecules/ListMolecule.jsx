d130af76a05f4fa2d9385b2a76680aaea63432df
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-03-16 14:49:28 +0100

    List molecule for rendering contact links

--- src/components/pages/molecules/ListMolecule.jsx
+++ src/components/pages/molecules/ListMolecule.jsx
@@ -1,8 +1,18 @@
-const ListMolecule = ({ items }) => {
-  const listItems = items.map(item => {
-    return <li>{item}</li>;
-  });
+import { ContactLinkAtom } from '../atoms/ContactLinkAtom';
 
+import { nanoid } from 'nanoid';
+
+const ListMolecule = items => {
+  const itemsArray = items.items;
+  const listItems = itemsArray.map(item => {
+    const { href, text } = item;
+    const id = nanoid();
+    return (
+      <li key={id}>
+        <ContactLinkAtom href={href} text={text} />
+      </li>
+    );
+  });
   return <ul>{listItems}</ul>;
 };
 
