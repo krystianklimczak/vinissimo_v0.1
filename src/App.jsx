f825145cf0fb76d7c5021775c26240fec0e56d3b
kklimczak <kklimczak7@icloud.com>
2024-03-24 22:32:48 +0100

    Implement shared layout + home as a main path and page404 route

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
-import { TestPage } from './components/pages/TestPage.jsx';
+
+import { Home } from './pages/Home/Home.jsx';
 import { Page404 } from './pages/Page404/Page404.jsx';
+import { SharedLayout } from './components/template/SharedLayout/SharedLayout.jsx';
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<TestPage />} />
-        <Route path="*" element={<Page404 />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+      </Route>
+      <Route path="*" element={<Page404 />} />
+    </Routes>
   );
 }
 
