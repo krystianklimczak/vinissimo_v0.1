20e5dababc88fb917a8c94fdb0aa982f676f9de3
kklimczak <kklimczak7@icloud.com>
2024-03-24 16:23:44 +0100

    Implement Routes&Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import { TestPage } from './components/pages/TestPage.jsx';
+import { Page404 } from './pages/Page404/Page404.jsx';
 
 function App() {
-  return <TestPage />;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<TestPage />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </div>
+  );
 }
 
 export { App };
