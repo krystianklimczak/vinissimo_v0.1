0de2fd33f6d87751b39334d6e0529af3cc4ba767
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-03-25 23:41:35 +0100

    Add div's for styling footer

--- src/components/template/Footer/Footer.jsx
+++ src/components/template/Footer/Footer.jsx
@@ -3,28 +3,38 @@ import css from './Footer.module.css';
 function Footer() {
   return (
     <footer className={css.footer}>
-      <p>Vinissimo Restaurant & Wine</p>
-      <p>Bar ul. Bema 6 Sopot</p>
-      <p>pon-sb: 14-22</p>
-      <p>niedziela: 13-19</p>
-      <p>tel. +48 881 334 449</p>
-      <p>booking@vinissimorestaurant.pl</p>
-      <img
-        style={{
-          width: '100px',
-          height: '100px',
-          background: 'black',
-        }}
-        alt="Vinissimo instagram"
-      />
-      <img
-        style={{
-          width: '100px',
-          height: '100px',
-          background: 'black',
-        }}
-        alt="Vinissimo facebook"
-      />
+      <div className={css.footer__links}>
+        <div>
+          <p>Vinissimo Restaurant & Wine</p>
+          <p>Bar ul. Bema 6 Sopot</p>
+        </div>
+        <div>
+          <p>pon-sb: 14-22</p>
+          <p>niedziela: 13-19</p>
+        </div>
+        <div>
+          <p>tel. +48 881 334 449</p>
+          <p>booking@vinissimorestaurant.pl</p>
+        </div>
+      </div>
+      <div className={css.footer__icons}>
+        <img
+          style={{
+            width: '100px',
+            height: '100px',
+            background: 'black',
+          }}
+          alt="Vinissimo instagram"
+        />
+        <img
+          style={{
+            width: '100px',
+            height: '100px',
+            background: 'black',
+          }}
+          alt="Vinissimo facebook"
+        />
+      </div>
     </footer>
   );
 }
