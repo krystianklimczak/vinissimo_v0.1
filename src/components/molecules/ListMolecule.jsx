7d27c374516aec4dba8538419dc1b1227eb32d7d
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-04-03 10:30:49 +0200

    Fix contactLinkAtom

--- src/components/molecules/ListMolecule.jsx
+++ src/components/molecules/ListMolecule.jsx
@@ -2,13 +2,19 @@ import { nanoid } from 'nanoid';
 
 import { ContactLinkAtom } from '../atoms/ContactLinkAtom/ContactLinkAtom.jsx';
 
+import css from './ListMolecule.module.css';
+
 function ListMolecule({ items }) {
   const listItems = items.map(item => {
     const { href, text } = item;
     const id = nanoid();
     return (
       <li key={id}>
-        <ContactLinkAtom href={href} text={text} />
+        <ContactLinkAtom
+          href={href}
+          text={text}
+          className={css.ContactLinkAtom}
+        />
       </li>
     );
   });
