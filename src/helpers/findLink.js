06cb65169578ef0fe0a5c4ce12944c2f421c95d3
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-04-05 11:43:17 +0200

    Add helpers

--- src/helpers/findLink.js
+++ src/helpers/findLink.js
@@ -4,9 +4,21 @@ function findLink(id) {
   return links.links.find(link => link.id === id);
 }
 
-const { href: hrefInstagram } = findLink('instagram');
-const { href: hrefFacebook } = findLink('facebook');
-const { href: hrefPhoneNumber } = findLink('phone');
 const { href: hrefEmail } = findLink('mail');
+const { href: hrefGoogle } = findLink('google');
+const { href: hrefPhoneNumber } = findLink('phone');
+const { href: hrefFacebook } = findLink('facebook');
+const { href: hrefInstagram } = findLink('instagram');
+const { href: hrefTrojmiasto } = findLink('trojmiasto');
+const { href: hrefTripAdvisor } = findLink('tripadvisor');
 
-export { findLink, hrefInstagram, hrefFacebook, hrefPhoneNumber, hrefEmail };
+export {
+  findLink,
+  hrefTripAdvisor,
+  hrefInstagram,
+  hrefFacebook,
+  hrefPhoneNumber,
+  hrefEmail,
+  hrefGoogle,
+  hrefTrojmiasto,
+};
