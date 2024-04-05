0d2428ae2a40698da9e89ed040e2358ccccbfb13
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-04-05 11:45:36 +0200

    Rate us on desktop version

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,19 @@
-import { About } from '../../components/template/About/About';
-import { MainBoard } from '../../components/template/MainBoard/MainBoard';
 import { Menu } from '../../components/template/Menu/Menu';
+import { About } from '../../components/template/About/About';
+import { RateUs } from '../../components/template/RateUs/RateUs';
+import { MainBoard } from '../../components/template/MainBoard/MainBoard';
 import { Reservation } from '../../components/template/Reservation/Reservation';
+import { CheckMediaQuery } from '../../helpers/checkMediaQuery';
 
 function Home() {
+  const screen = CheckMediaQuery();
   return (
     <div>
       <MainBoard />
       <About />
       <Menu />
       <Reservation />
+      {screen === 'desktop' && <RateUs />}
     </div>
   );
 }
