f8c07834bd1b0abcf5ecd2ff83b48e725c728cda
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-04-06 11:14:12 +0200

    Fix structure of buttons

--- src/components/template/Menu/Menu.jsx
+++ src/components/template/Menu/Menu.jsx
@@ -9,25 +9,32 @@ function Menu() {
   const screen = CheckMediaQuery();
   return (
     <Section title="menu">
-      <h3>przystawki</h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure corporis
-        iusto ipsam ipsum perferendis, enim placeat similique doloribus totam
-        veniam porro at autem rem pariatur facere sint suscipit esse dolores
-        obcaecati quas ea corrupti neque repudiandae quod? Laboriosam
-        architecto, ex sint eum sapiente temporibus animi accusamus quis quaerat
-        soluta dolores!
-      </p>
+      <div className={css.menu}>
+        <h3>przystawki</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure corporis
+          iusto ipsam ipsum perferendis, enim placeat similique doloribus totam
+          veniam porro at autem rem pariatur facere sint suscipit esse dolores
+          obcaecati quas ea corrupti neque repudiandae quod? Laboriosam
+          architecto, ex sint eum sapiente temporibus animi accusamus quis
+          quaerat soluta dolores!
+        </p>
 
-      {screen === 'desktop' ? (
         <div className={css.menu__buttons}>
-          <Button text="wina" />
-          <Button text="wina na kieliszki" />
-          <Button text="alkohole" />
+          {screen === 'desktop' ? (
+            <>
+              <Button text="wina" />
+              <Button text="wina na kieliszki" />
+              <Button text="alkohole" />
+            </>
+          ) : (
+            <Button
+              text={<span>Więcej</span>}
+              icon={<Icon id="arrow-down" />}
+            />
+          )}
         </div>
-      ) : (
-        <Button text={<span>Więcej</span>} icon={<Icon id="arrow-down" />} />
-      )}
+      </div>
     </Section>
   );
 }
